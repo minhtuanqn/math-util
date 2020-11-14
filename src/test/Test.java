220322339a1c56a0b70fc1d5203ac7d349216a1b
minhtuanqn <minhtuanqn320@gmail.com>
2020-11-14 12:12:39 +0700

    2nd: import static modification

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,8 +5,9 @@
  */
 package test;
 
-import tuanlm.util.MathUtility;
-
+import static tuanlm.util.MathUtility.*;
+//cau lenh moi tu jdk 8
+//o duoi goi ham static khong can cham, y chang c , chi ao dung static
 /**
  *
  * @author MINH TUAN
@@ -16,16 +17,19 @@ public class Test {
         //ta test thu ham cua ta
         //dung mat de so sanh ket qua thuc te khi chay va key qua ta mong doi
         long expected = 120; //ta mong nhan 120 neu goi ham 5!
-        long actual = MathUtility.getFactorial(5);
+        long actual = getFactorial(5);
         System.out.println("5!: Expected: " + expected + ";Actual: " + actual);
         
         //expected: 710 if tinh 6!
-        System.out.println("6!: " + MathUtility.getFactorial(6));
+        System.out.println("6!: " + getFactorial(6));
         
         //expected: 1 if tinh 0!
-        System.out.println("0!: " + MathUtility.getFactorial(0));
+        System.out.println("0!: " + getFactorial(0));
         
         //expected: IllegalArgumentException if tinh -5
-        System.out.println("-5!: " + MathUtility.getFactorial(-5));
+        System.out.println("-5!: " + getFactorial(-5));
+        
+        //in thu PI coi co giong MATH.PI
+        System.out.println("PI: " + PI);
     }
 }
